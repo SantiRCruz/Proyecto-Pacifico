3a821a8e587925a27d718a15a2355428a388b16b
GuillermoSamboni <guillermosamboni0912@gmail.com>
2022-06-23 17:34:50 -0500

    contruccion3 adelanto

---
+++ app/src/main/java/com/fabricaswsenactpi/com/construyendopacifico/ui/user/menuModules/constructionUser/ConstructionFragment3.kt
@@ -0,0 +1,24 @@
+package com.fabricaswsenactpi.com.construyendopacifico.ui.user.menuModules.constructionUser
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fabricaswsenactpi.com.construyendopacifico.R
+import com.fabricaswsenactpi.com.construyendopacifico.databinding.FragmentConstructionBinding
+
+class ConstructionFragment3 : Fragment(R.layout.fragment_construction3) {
+    private lateinit var binding: FragmentConstructionBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentConstructionBinding.bind(view)
+
+        clicks()
+
+    }
+
+    private fun clicks() {
+    }
+
+}
