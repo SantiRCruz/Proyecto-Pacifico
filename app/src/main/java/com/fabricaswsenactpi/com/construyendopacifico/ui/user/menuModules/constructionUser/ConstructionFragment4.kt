f8fe172919a5dfc7369b29e4473197e800d0e8e3
GuillermoSamboni <guillermosamboni0912@gmail.com>
2022-06-24 13:50:42 -0500

    terminado3

---
+++ app/src/main/java/com/fabricaswsenactpi/com/construyendopacifico/ui/user/menuModules/constructionUser/ConstructionFragment4.kt
@@ -0,0 +1,25 @@
+package com.fabricaswsenactpi.com.construyendopacifico.ui.user.menuModules.constructionUser
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fabricaswsenactpi.com.construyendopacifico.R
+import com.fabricaswsenactpi.com.construyendopacifico.databinding.FragmentConstructionBinding
+
+
+class ConstructionFragment4 : Fragment(R.layout.fragment_construction4) {
+    private lateinit var binding: FragmentConstructionBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentConstructionBinding.bind(view)
+
+        clicks()
+
+    }
+
+    private fun clicks() {
+    }
+
+}
